73d46dbd418274d937c1ac5bc6d8ae26ecc01203
Vibhor Kukreja <vibhor.kukreja@tothenew.com>
2017-08-18 23:29:40 +0530

    Updated the direct exchange.

--- direct/recieve.js
+++ direct/recieve.js
@@ -1,25 +1,22 @@
-/**
- * Created by mayank on 15/8/17.
- */
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
 //our tcp connection to rabbitmq
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect('amqp://localhost', (err, conn) => {
 
     //creating channel
-    conn.createChannel(function(err, ch) {
+    conn.createChannel((err, ch) => {
 
-        var queueName = "foo";
+        let queueName = "direct-queue";
 
         //assert your queue either to persist in memory or not
         ch.assertQueue(queueName, {durable: false});
 
 
-        console.log(" [x] I am Waiting for you message...");
+        console.log(" I am Waiting for you message...");
 
         //to comsume message sent to the queue 'foo'
-        ch.consume(queueName, function(msg) {
+        ch.consume(queueName, msg => {
             console.log(" Received a message: ", msg.content.toString());
         }, {noAck: true});
 
--- direct/send.js
+++ direct/send.js
@@ -1,25 +1,27 @@
-/**
- * Created by mayank on 15/8/17.
- */
-var amqp = require('amqplib/callback_api');
+
+const amqp = require('amqplib/callback_api');
 
 //our tcp connection to rabbitmq
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect('amqp://localhost', (err, conn) => {
 
     //creating channel
-    conn.createChannel(function(err, ch) {
+    conn.createChannel((err, ch) => {
 
-        var queueName = "foo"
-            , message = 'Hello World!'
-        ;
+        let queueName = "direct-queue"
+            , message = +new Date() + ' I started with nothing, and I still have most of it.';
+
 
         //assert your queue either to persist in memory or not
         ch.assertQueue(queueName, {durable: false});
 
+
+
         //send the message to queue
         ch.sendToQueue(queueName, new Buffer(message));
 
-        console.log(" [x] Sent", message);
+
+        console.log("Your message has been send successfully.");
+
 
         //Task completed close the connection and exit the process
         setTimeout(function() { conn.close(); process.exit(0) }, 500);
